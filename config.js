4b2c708b902c74fa695ea5f7e941aa58e53996ec
Tritoapriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2021-09-06 14:42:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,47 @@
+global.DeveloperMode = 'True' //Buat Mode True False Moderator
+global.linkGC = ['https://chat.whatsapp.com/CAKETl3LJdELqHTqOJtH4S']
+global.channelYT = ['Fernazer'] 
+global.owner = ['6285888258313']// Nomer Owner Utama
+global.mods = ['6289607997717'] // Buat Owner Biar Unlimitid
+global.prems = [] // Buat Yg User Premium 
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Bree Isi Di Sini
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'HIRO', 
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ' 
+}
+
+// Sticker WM
+global.packname = 'LOLITA BOT'
+global.author = 'BOT BY TRITO'
+
+global.multiplier = 20 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
